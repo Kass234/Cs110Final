3165baa6c7f36df5cf3722a8700fcc6121195598
vTHEBIN <60268456+vTHEBIN@users.noreply.github.com>
2020-04-21 16:39:45 -0400

    Update PingPong.py

--- PingPong.py
+++ PingPong.py
@@ -1,1 +1,15 @@
+import random
+import pygame
+from pygame.math import Vector2
+pygame.init()
+
+FONT = pygame.font.SysFont('Helvetica', 25)
+FPS = 30
+WIN_WIDTH = 800
+WIN_HEIGHT = 640
+MAX_SCORE = 5
+DISPLAY = (WIN_WIDTH, WIN_HEIGHT)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GRAY = (64, 64, 64)
 
